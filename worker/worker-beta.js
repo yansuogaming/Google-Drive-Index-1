f3e71a7394f6150794a607f646728d401cb8b29d
Saksham Shekher <67940483+Oshekher@users.noreply.github.com>
2021-06-29 01:32:07 +0530

    Update worker-beta.js

--- worker/worker-beta.js
+++ worker/worker-beta.js
@@ -57,16 +57,16 @@ const authConfig = {
 
 const uiConfig = {
     "theme": "slate", // switch between themes, default set to vapor, select from https://www.npmjs.com/package/@googledrive/index
-    "version": "2.0.20", // don't touch this one. get latest code using generator at https://bdi-generator.hashhackers.com
+    "version": "2.0.21", // don't touch this one. get latest code using generator at https://bdi-generator.hashhackers.com
     // If you're using Image then set to true, If you want text then set it to false
     "logo_image": true, // true if you're using image link in next option.
     "logo_height": "", // only if logo_image is true
     "logo_width": "100px", // only if logo_image is true
     "favicon": "https://cdn.jsdelivr.net/gh/OneFusionPlus/Google-Drive-Index@2.0.20/images/favicon.ico",
     // if logo is true then link otherwise just text for name
-    "logo_link_name": "https://github.com/OneFusionPlus/Google-Drive-Index/blob/master/images/academy.png",
-    "fixed_header": true, // If you want the footer to be flexible or fixed.
-    "header_padding": "60", // Value 60 for fixed header, Value 20 for flexible header. Required to be changed accordingly in some themes.
+    "logo_link_name": "https://prod-ak-abs-na-web-assets.erosnow.com/production/erosnow-logo656b74dd1f76aad2e8e9083d1b11f1aa.svg",
+    "fixed_header": false, // If you want the footer to be flexible or fixed.
+    "header_padding": "20", // Value 60 for fixed header, Value 20 for flexible header. Required to be changed accordingly in some themes.
     "nav_link_1": "Home", // change navigation link name
     "nav_link_3": "Current Path", // change navigation link name
     "nav_link_4": "Contact", // change navigation link name
